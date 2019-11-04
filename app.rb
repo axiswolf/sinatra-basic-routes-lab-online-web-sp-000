d816aa21f6b97e0ff33e5e50ff93597329e18491
Karen V <karenvo@usc.edu>
2019-11-04 13:18:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    puts "My name is "
+    "My name is Karen"
   end
 
   get '/hometown' do
-    puts "My hometown is Burbank"
+    "My hometown is Burbank"
   end
 
   get '/favorite-song' do
-    puts "My favorite song is Helena"
+    "My favorite song is Helena"
   end
 end
